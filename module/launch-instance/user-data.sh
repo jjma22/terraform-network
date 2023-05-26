71b380f439b29139a91f131a29b310e8dae11e03
jjma22 <joe.adams123@hotmail.co.uk>
2023-05-26 11:07:07 +0100

    configration for git clone test in nginx

--- module/launch-instance/user-data.sh
+++ module/launch-instance/user-data.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install nginx -y
-sudo ufw allow 'Nginx HTTP'
-yes | sudo ufw enable
-cd /var/www
-sudo mkdir tutorial
-cd tutorial
-sudo "${EDITOR:-vi}" index.html
+
+
+# clone repo 
+# move index.html to new location 
